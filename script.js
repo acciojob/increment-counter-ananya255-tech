4dbe15015b9a844d3593efc858ee19241bb73168
Ananya Virwani <ananyavirwani255@gmail.com>
2026-09-24 18:00:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let counter=0
+function increment(){
+		counter++;
+	console.log(counter)
+}
+
+let mybtn=document.getElementByID("btn")
+mybtn.addEventListener("click",increment)
+}
